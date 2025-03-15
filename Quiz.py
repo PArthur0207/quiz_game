d630aaac92f8c98f600fe795e9c6a722d2494334
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-15 08:36:01 +0800

    Pseudocode

---
+++ Quiz.py
@@ -0,0 +1,6 @@
+# Initialize text file containing quiz questions and answers
+# Make this code access the text file
+# Multiple choice picking from a-d
+# Make code shorter
+# Tracks the test scores
+# Show text scores
