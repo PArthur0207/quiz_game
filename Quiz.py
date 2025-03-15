f5717fd3eb451633874dd0d4d9276be2089429b0
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-15 09:28:48 +0800

    Prints the first questions and asks for answer correctly

--- Quiz.py
+++ Quiz.py
@@ -3,10 +3,22 @@
 quiz_file = open("the_quiz.txt", "r")
 lines = quiz_file.read().splitlines()
 print(lines[0])
-quiz_file.close()
+print(lines[2])
+print(lines[3])
+print(lines[4])
+print(lines[5])
+print(lines[6])
+
+answer = input("Answer here: ").strip().upper()
+correct = lines[8].strip()
+print(correct)
+if answer == correct:
+    print("Correct!")
+else:
+    print("Wrong!")
 # Multiple choice picking from a-d
 # Make code shorter
 # Tracks the test scores
 # Show text scores
 
-
+quiz_file.close()
