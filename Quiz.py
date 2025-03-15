54c732ffa556452e49a71b7a123b6c6894e0abcd
aluckytoothpick <marayanpaularthur1114@gmail.com>
2025-03-15 09:13:27 +0800

    Fix the text file and access the txt file from the py code

--- Quiz.py
+++ Quiz.py
@@ -1,6 +1,12 @@
 # Initialize text file containing quiz questions and answers
 # Make this code access the text file
+quiz_file = open("the_quiz.txt", "r")
+lines = quiz_file.read().splitlines()
+print(lines[0])
+quiz_file.close()
 # Multiple choice picking from a-d
 # Make code shorter
 # Tracks the test scores
 # Show text scores
+
+
